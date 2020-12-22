d01c0d24040cc776eb5769131b22a9fdbe5cd005
joelkansala <joelkansala@kamk.fi>
2020-12-22 21:59:46 +0200

    day1 complete

---
+++ day1.py
@@ -0,0 +1,97 @@
+
+'''
+expense_report takes an list of integer values
+as data. It finds two values in that list that
+add to 2020 and multiplies them together.
+Then it returns the value
+'''
+def expense_report(parameter):
+    if (parameter == None):
+        raise TypeError("data is empty or not defined")
+    if (type(parameter) != list and type(parameter) != str):
+        raise TypeError("input value is not a list or string.")
+    if (len(parameter) <= 1):
+        raise AssertionError("List must have more than 1 value")
+
+    reportData = []
+    if (type(parameter) == str):
+        reportData = read_file(parameter)
+    else:
+        reportData = parameter
+
+    # Find Loop through the values in two iterators
+    valueFound = -1
+    firstValue = -1
+    secondValue = -1
+    for i in range(0, len(reportData) - 1):
+        for j in range(i+1, len(reportData)):
+            if reportData[i] + reportData[j] == 2020:
+                firstValue = reportData[i]
+                secondValue = reportData[j]
+                valueFound = firstValue * secondValue
+                break
+            else:
+                continue
+
+    if valueFound > 0:
+        print(f"Sum 2020 found with values {firstValue} and {secondValue}.")
+        print(f"Final value was {valueFound}")
+        return valueFound
+    else:
+        print("no two values with sum equal to 2020 found.")
+        return -1
+
+def expense_report_advanced(parameter):
+    if (parameter == None):
+        raise TypeError("data is empty or not defined")
+    if (type(parameter) != list and type(parameter) != str):
+        raise TypeError("input value is not a list or string.")
+    if (len(parameter) <= 2):
+        raise AssertionError("List must have more than 2 values")
+
+    reportData = []
+    if (type(parameter) == str):
+        reportData = read_file(parameter)
+    else:
+        reportData = parameter
+
+    # Find Loop through the values in two iterators
+    values = {"first":-1, "second":-1, "third":-1, "answer":-1}
+    for i in range(0, len(reportData)):
+        for j in range(i+1, len(reportData)):
+            for k in range(j+1, len(reportData)):
+                if reportData[i] + reportData[j] + reportData[k] == 2020:
+                    values["first"]  = reportData[i]
+                    values["second"] = reportData[j]
+                    values["third"]  = reportData[k]
+                    values["answer"] = values["first"] * values["second"] * values["third"]
+                    break
+                else:
+                    continue
+
+    if values["answer"] > 0:
+        print(f"Sum 2020 found with values {values['first']}, {values['second']} and {values['third']}.")
+        print(f"Final value was {values['answer']}")
+        return values["answer"]
+    else:
+        print("no three values with sum equal to 2020 found.")
+        return 0
+
+def read_file(filename):
+    print(f"Reading file {filename}")
+    content = []
+    try:
+        with open(filename, "r") as f:
+            for line in f:
+                print(f"> {line}")
+                content.append(int(line.strip()))
+    except FileNotFoundError:
+        raise FileNotFoundError("File not found")
+
+    if (len(content) <= 1):
+        raise AssertionError("Reading file fail: Data List too short")
+    return content
+
+if __name__ == "__main__":
+    expense_report_advanced("day1_input.txt")
+    pass
---
+++ test_day1.py
@@ -0,0 +1,26 @@
+import unittest
+from day1 import expense_report, expense_report_advanced
+
+class TestDay1_1(unittest.TestCase):
+    def test_types(self):
+        self.assertRaises(TypeError, expense_report, None)
+        self.assertRaises(TypeError, expense_report, 1234)
+        # Day 2 problem tests
+        self.assertRaises(TypeError, expense_report_advanced, None)
+        self.assertRaises(TypeError, expense_report_advanced, 1234)
+
+    def test_input(self):
+        oneVal = [1]
+        self.assertRaises(AssertionError, expense_report, oneVal)
+        self.assertRaises(AssertionError, expense_report_advanced, oneVal)
+        twoValues =  [1010, 1010]
+        self.assertRaises(AssertionError, expense_report_advanced, twoValues)
+        testString = "notfound.txt"
+        self.assertRaises(FileNotFoundError, expense_report_advanced, testString)
+
+    def test_values(self):
+        twoValues = [1010, 1010]
+        self.assertEqual(expense_report(twoValues), 1010*1010)
+        basicData = [1721, 979, 366, 299, 675, 1456]
+        self.assertEqual(expense_report(basicData), 514579)
+        self.assertEqual(expense_report_advanced(basicData), 241861950)
